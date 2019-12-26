82033db3edf96e99f93c64eb96bb45c4d2a63000
Khammai <khammaics@gamil.com>
2019-12-26 08:42:52 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <Navbar />
+    <v-content />
+    <router-view> </router-view>
+    <Footer />
+  </v-app>
 </template>
 
+<script>
+import Navbar from "./views/Navbar";
+import Footer from "./views/Footer";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.font {
+  font-family: "Noto Sans Lao UI", "Bahnschrift Light";
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.mr-2 {
+  font-size: 16px;
+  color: #ffffff;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,70 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import i18n from "../i18n";
 import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
+// import Inspection from "../views/Inspection.vue";
+// import Register from "../views/Register.vue";
+// import Inspections from "../views/Inspections.vue";
+// import User from "../views/User.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    redirect: `/${i18n.locale}`
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/:lang",
+    component: {
+      render(c) {
+        return c("router-view");
+      }
+    },
+    children: [
+      {
+        path: "/",
+        name: "home",
+        component: Home
+      },
+      {
+        path: "Register",
+        name: "/Register",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/Register.vue")
+      },
+      {
+        path: "Inspection",
+        name: "/Inspection",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/Inspection.vue")
+      },
+      {
+        path: "Inspections",
+        name: "/Inspections",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/Inspections.vue")
+      },
+      {
+        path: "Service",
+        name: "/Service",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/Service.vue")
+      },
+      {
+        path: "User",
+        name: "/User",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/User.vue")
+      },
+      {
+        path: "Login",
+        name: "/Login",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "../views/Login.vue")
+      }
+    ]
   }
 ];
 
